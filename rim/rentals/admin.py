0e3c5e86725357b44f1a744c5b2c4a723f1a744b
Ryan Buchmeier <rwbleo@gmail.com>
2017-11-30 10:46:18 -0700

    Add Django Admin for models

--- rim/rentals/admin.py
+++ rim/rentals/admin.py
@@ -3,4 +3,20 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import Person
+from .models import Rental
+from .models import Gear
+from .models import PersonName
+from .models import Email
+from .models import Phone
+from .models import Relationship
+
 # Register your models here.
+admin.site.register(Person)
+admin.site.register(Rental)
+admin.site.register(Gear)
+admin.site.register(PersonName)
+admin.site.register(Email)
+admin.site.register(Phone)
+admin.site.register(Relationship)
+
